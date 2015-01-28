cd3ec049c80834c776d3d7982aa126efbeedc385
Stefano Monni <stefano.monni@crs4.it>
2015-01-28 19:48:24 +0100

    added temporary changes

--- eco_app/src/most/demo/ecoapp/TeleconsultationActivity.java
+++ eco_app/src/most/demo/ecoapp/TeleconsultationActivity.java
@@ -11,6 +11,7 @@ import org.crs4.most.visualization.StreamViewerFragment;
 
 import most.demo.ecoapp.models.EcoUser;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -31,26 +32,35 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 	private IStream stream1 = null;
 	private Handler handler;
 	
+	
+	private ProgressDialog progressWaitingSpec;
+	
+	private TeleconsultationState tcState = TeleconsultationState.READY;
+	private TeleconsultationStateView tcStateView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		Intent i = getIntent();
-		this.ecoUser =  (EcoUser) i.getExtras().getSerializable("EcoUser");
+		// Intent i = getIntent();
+		//this.ecoUser =  (EcoUser) i.getExtras().getSerializable("EcoUser");
 		
-		setContentView(R.layout.activity_teleconsultation);
-		if (savedInstanceState == null) {
-	        
-			String streamName = "Teleconsultation Stream";
-			String streamUri = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov";
+		    setContentView(R.layout.activity_teleconsultation);
+	         
+		    //this.waitForSpecialist();
+		    
+		    this.tcStateView = (TeleconsultationStateView) findViewById(R.id.tcStateView);
+		    this.tcStateView.setTeleconsultationState(this.tcState);
+		    
+			this.handler = new Handler(this);
+			 
+			String streamName = "Teleconsultation Stream"; 
+			String streamUri =  "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov";
+			
+			this.prepareStream(streamName,streamUri);
+			
 			this.stream1Fragment = StreamViewerFragment.newInstance(streamName);
 			
-			 this.handler = new Handler(this);
-		    	
-		    	
-		    // Instance and initialize the Streaming Library
-		    	
-		   
 			// add the first fragment to the first container
 	    	FragmentTransaction fragmentTransaction = getFragmentManager()
 					.beginTransaction();
@@ -58,8 +68,6 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 					stream1Fragment);
 			fragmentTransaction.commit();
 			
-			this.prepareStream(streamName,streamUri);
-		}
 	}
 
 	
@@ -77,7 +85,7 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 				
 			this.stream1 = streamingLib.createStream(stream1_params, this.handler);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			Log.e(TAG,"ERROR INITIALIZING STREAM:" + e);
 			e.printStackTrace();
 		}
 		    
@@ -117,6 +125,7 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 
 	@Override
 	public void onSurfaceViewCreated(String streamId, SurfaceView surfaceView) {
+		Log.d(TAG, "Surface View created: preparing surface for stream" + streamId);
 		this.stream1.prepare(surfaceView);
 		this.stream1Fragment.setPlayerButtonsVisible(true);
 		
@@ -131,23 +140,24 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 	public boolean handleMessage(Message streamingMessage) {
 		StreamingEventBundle myEvent = (StreamingEventBundle) streamingMessage.obj;
 		
-		Log.d(TAG, "Stream Event Name:" + myEvent.getEvent().name());
-		Log.d(TAG, "Stream Event Info:" + myEvent.getInfo());
+		String infoMsg ="Event Type:" +  myEvent.getEventType() + " ->" +  myEvent.getEvent() + ":" + myEvent.getInfo();
+		Log.d(TAG, "handleMessage: Current Event:" + infoMsg);
 		Log.d(TAG, "Stream State:" + this.stream1.getState());
 		return false;
 	}
 	
 	
 	
-//	private void waitForSpecialist()
-//	{
-//		//Toast.makeText(MainActivity.this, "Connecting to:" + deviceName + "(" + macAddress +")" , Toast.LENGTH_LONG).show();
-//		progressWaitingSpec = new ProgressDialog(TeleconsultationActivity.this);
-//		progressWaitingSpec.setTitle("Preparing Teleconsultation Session");
-//		progressWaitingSpec.setMessage("Waiting for specialist...");
-//		
-//		progressWaitingSpec.show();
-//		
-//	}
+	private void waitForSpecialist()
+	{
+		//Toast.makeText(MainActivity.this, "Connecting to:" + deviceName + "(" + macAddress +")" , Toast.LENGTH_LONG).show();
+		progressWaitingSpec = new ProgressDialog(TeleconsultationActivity.this);
+		progressWaitingSpec.setTitle("Preparing Teleconsultation Session");
+		progressWaitingSpec.setMessage("Waiting for specialist...");
+		progressWaitingSpec.setCancelable(false);
+		progressWaitingSpec.setCanceledOnTouchOutside(false);
+		progressWaitingSpec.show();
+		
+	}
 	
 }
