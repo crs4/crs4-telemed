5726cf2b8cad6150c2593ade16f7e4f79e02e322
Stefano Monni <stefano.monni@crs4.it>
2015-01-30 18:31:29 +0100

    added voip to eco app

--- eco_app/src/most/demo/ecoapp/TeleconsultationActivity.java
+++ eco_app/src/most/demo/ecoapp/TeleconsultationActivity.java
@@ -11,8 +11,15 @@ import org.crs4.most.visualization.StreamViewerFragment;
 
 import most.demo.ecoapp.models.EcoUser;
 import most.demo.ecoapp.ui.TcStateTextView;
+import most.voip.api.Utils;
+import most.voip.api.VoipEventBundle;
+import most.voip.api.VoipLib;
+import most.voip.api.VoipLibBackend;
+import most.voip.api.enums.VoipEvent;
+import most.voip.api.enums.VoipEventType;
 import android.annotation.SuppressLint;
 import android.app.ActionBar.LayoutParams;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -51,9 +58,18 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 	
 	private ProgressDialog progressWaitingSpec;
 	
-	private TeleconsultationState tcState = TeleconsultationState.READY;
+	private TeleconsultationState tcState = TeleconsultationState.IDLE;
 	private TcStateTextView txtTcState = null;
 	private ImageButton butCall;
+	private String sipServerIp;
+	private String sipServerPort;
+	private String accountName;
+	private VoipLib myVoip;
+	private CallHandler voipHandler;
+	private PopupWindow popupWindow;
+	private Button popupCancelButton;
+	private Button popupHangupButton;
+	private Button popupHoldButton;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -62,32 +78,77 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 		// Intent i = getIntent();
 		//this.ecoUser =  (EcoUser) i.getExtras().getSerializable("EcoUser");
 		
+		
 		    setContentView(R.layout.activity_teleconsultation);
-	        txtTcState = (TcStateTextView) findViewById(R.id.txtTcState);
-	        txtTcState.setTeleconsultationState(this.tcState);
-		    //this.waitForSpecialist();
-
-		    
+		    txtTcState = (TcStateTextView) findViewById(R.id.txtTcState);
+	       
+		   
 			this.handler = new Handler(this);
-			 
-			String streamName = "Teleconsultation Stream"; 
-			String streamUri =  "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov";
-			
-			this.prepareStream(streamName,streamUri);
-			
-			this.stream1Fragment = StreamViewerFragment.newInstance(streamName);
-			
-			// add the first fragment to the first container
-	    	FragmentTransaction fragmentTransaction = getFragmentManager()
-					.beginTransaction();
-			fragmentTransaction.add(R.id.container_stream,
-					stream1Fragment);
-			fragmentTransaction.commit();
 			
 			this.setupActionBar();
+			this.setupCallPopupWindow();
+			this.setTeleconsultationState(TeleconsultationState.IDLE);
 			
+			this.setupStreamLib();
+			this.setupVoipLib();
+			
+			
+			 //this.waitForSpecialist();
 	}
-
+    
+	
+	private void setTeleconsultationState(TeleconsultationState tcState)
+	{
+		this.tcState = tcState; 
+		notifyTeleconsultationStateChanched();
+		
+	}
+	
+	private void notifyTeleconsultationStateChanched() {
+		
+		txtTcState.setTeleconsultationState(this.tcState);
+		if (this.tcState==TeleconsultationState.IDLE || tcState==TeleconsultationState.READY)
+		{
+			butCall.setEnabled(false);
+			popupCancelButton.setEnabled(true);
+			popupHoldButton.setEnabled(false);
+			popupHangupButton.setEnabled(false);
+		}
+		else if (this.tcState==TeleconsultationState.CALLING)
+		{
+			butCall.setEnabled(true);
+			popupCancelButton.setEnabled(true);
+			popupHoldButton.setEnabled(true);
+			popupHangupButton.setEnabled(true);
+		}
+		
+		else if (this.tcState==TeleconsultationState.HOLDING)
+		{
+			butCall.setEnabled(true);
+			popupCancelButton.setEnabled(true);
+			popupHoldButton.setEnabled(true);
+			popupHoldButton.setText("Unhold"); // to be fixed
+			popupHangupButton.setEnabled(true);
+		}
+		 
+	}
+	
+	private void setupStreamLib()
+	{
+		String streamName = "Teleconsultation Stream"; 
+		String streamUri =  "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov";
+		
+		this.prepareStream(streamName,streamUri);
+		
+		this.stream1Fragment = StreamViewerFragment.newInstance(streamName);
+		
+		// add the first fragment to the first container
+    	FragmentTransaction fragmentTransaction = getFragmentManager()
+				.beginTransaction();
+		fragmentTransaction.add(R.id.container_stream,
+				stream1Fragment);
+		fragmentTransaction.commit();
+	}
 	
 	private void prepareStream(String name, String uri)
 	{
@@ -107,8 +168,10 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 			e.printStackTrace();
 		}
 		    
-	
 	}
+	
+	
+	
 	
 	private void setupActionBar()
 	{
@@ -158,27 +221,62 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 				 
 		 View popupView = inflater.inflate(R.layout.popup_call_selection,
 				 null);
+		this.popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+	}
+	
+	private void setupCallPopupWindow()
+	{
+		
+		 LayoutInflater inflater = (LayoutInflater)  TeleconsultationActivity.this
+				 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 				 
+		 View popupView = inflater.inflate(R.layout.popup_call_selection,
+				 null);	 
        
-        final PopupWindow popupWindow = new PopupWindow(popupView,
-                LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, true);
-        
-    
-        Button cancelButton = (Button) popupView.findViewById(R.id.butCallCancel);
-        
-        cancelButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				popupWindow.dismiss();
+		if (popupWindow==null)
+		{
+	        popupWindow = new PopupWindow(popupView,
+	                LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, true);
+	        
+	    
+	        popupCancelButton = (Button) popupView.findViewById(R.id.butCallCancel);
+	        
+	        popupCancelButton.setOnClickListener(new OnClickListener() {
 				
-			}
-		});
+				@Override
+				public void onClick(View v) {
+					popupWindow.dismiss();
+					
+				}
+			});
+	        
+	        popupHoldButton = (Button) popupView.findViewById(R.id.butCallHold);
+	        
+	        popupHoldButton.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// to be implemented
+					popupWindow.dismiss();
+				}
+			});
+	        
+	        popupHangupButton = (Button) popupView.findViewById(R.id.butCallHangup);
+	        
+	        popupHangupButton.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					hangupCall();
+					popupWindow.dismiss();
+				}
+			});
+	        
+	        popupWindow.setTouchable(true);
+	        popupWindow.setFocusable(true);
+		}
+		
         
-        popupWindow.setTouchable(true);
-        popupWindow.setFocusable(true);
-
-        popupWindow.showAtLocation(popupView, Gravity.BOTTOM, 0, 0);
        
 	}
 	
@@ -229,5 +327,201 @@ public class TeleconsultationActivity extends ActionBarActivity implements Handl
 		progressWaitingSpec.show();
 		
 	}
+	
+// VOIP METHODS AND LOGIC
+	
+	private class CallHandler extends Handler {
+
+		private TeleconsultationActivity app;
+		private VoipLib myVoip;
+		public boolean reinitRequest = false;
+		private boolean incoming_call_request;
+
+		public CallHandler(TeleconsultationActivity teleconsultationActivity,
+				VoipLib myVoip) {
+			this.app = teleconsultationActivity;
+			this.myVoip = myVoip;
+		}
+		
+		protected VoipEventBundle getEventBundle(Message voipMessage)
+ 		{
+ 			//int msg_type = voipMessage.what;
+			VoipEventBundle myState = (VoipEventBundle) voipMessage.obj;
+			String infoMsg = "Event:" + myState.getEvent() + ": Type:"  + myState.getEventType() + " : " + myState.getInfo();
+			Log.d(TAG, "Called handleMessage with event info:" + infoMsg);
+			return myState;
+ 		}
+		
+		@Override
+		public void handleMessage(Message voipMessage) {
+		
+			
+			VoipEventBundle myEventBundle = getEventBundle(voipMessage);
+			Log.d(TAG, "HANDLE EVENT TYPE:" + myEventBundle.getEventType() + " EVENT:" + myEventBundle.getEvent());
+			
+			
+	
+			//if (myEventBundle.getEventType()==VoipEventType.BUDDY_EVENT){}
+			
+			// Register the account after the Lib Initialization
+			if (myEventBundle.getEvent()==VoipEvent.LIB_INITIALIZED)   {myVoip.registerAccount();
+																			}	
+			else if (myEventBundle.getEvent()==VoipEvent.ACCOUNT_REGISTERED)    {
+																     this.app.setTeleconsultationState(TeleconsultationState.READY);
+			                                                      }	
+			
+			
+			else if (myEventBundle.getEvent()==VoipEvent.CALL_INCOMING)  handleIncomingCallRequest();
+			
+			else if (myEventBundle.getEvent()==VoipEvent.CALL_READY)
+			{
+				if (incoming_call_request)
+				{
+					answerCall();
+				}
+			}
+			
+			else if  (myEventBundle.getEvent()==VoipEvent.CALL_ACTIVE)    {
+				this.app.setTeleconsultationState(TeleconsultationState.CALLING);
+			}
+			
+		 
+			
+		
+			else if (myEventBundle.getEvent()==VoipEvent.CALL_HANGUP)    {
+				this.app.setTeleconsultationState(TeleconsultationState.READY);
+			}
+			// Deinitialize the Voip Lib and release all allocated resources
+			else if (myEventBundle.getEvent()==VoipEvent.LIB_DEINITIALIZED || myEventBundle.getEvent()==VoipEvent.LIB_DEINITIALIZATION_FAILED) 
+			{
+				Log.d(TAG,"Setting to null MyVoipLib");
+				this.app.myVoip = null;
+				this.app.setTeleconsultationState(TeleconsultationState.IDLE);
+				
+				if (this.reinitRequest)
+				{	this.reinitRequest = false;
+					this.app.setupVoipLib();
+				}
+			}
+			else showUnhandledEventAlert(myEventBundle);
+			     
+		} // end of handleMessage()
+
+		
+		private void showUnhandledEventAlert(VoipEventBundle myEventBundle) {
+		
+			AlertDialog.Builder miaAlert = new AlertDialog.Builder(this.app);
+			miaAlert.setTitle(myEventBundle.getEventType() + ":" + myEventBundle.getEvent());
+			miaAlert.setMessage(myEventBundle.getInfo());
+			AlertDialog alert = miaAlert.create();
+			alert.show();
+		}
+
+		private void handleIncomingCallRequest() {
+			
+			incoming_call_request = true;
+	
+		}	
+	}
+	
+	private void answerCall() {
+		myVoip.answerCall();
+		
+		/*
+		Log.d(TAG, "Answering the call after 2 seconds");
+		
+		final Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+		  @Override
+		  public void run() {
+			  Log.d(TAG, "Answering the call now...");
+			  myVoip.answerCall();
+		  }
+		}, 2000);
+		*/
+	}
+	
+	private void toggleHoldCall(boolean holding)
+	{
+		if (holding)
+			myVoip.holdCall();
+		else
+			myVoip.unholdCall();
+	}
+	
+	private void hangupCall()
+	{
+		myVoip.hangupCall();
+	}
+	
+	private void setupVoipLib()
+	{
+		// Voip Lib Initialization Params
+
+				HashMap<String,String> params = getVoipSetupParams();
+				
+				Log.d(TAG, "Initializing the lib...");
+				if (myVoip==null)
+				{
+					Log.d(TAG,"Voip null... Initialization.....");
+					myVoip = new  VoipLibBackend();
+					this.voipHandler = new CallHandler(this, myVoip);
+					
+					// Initialize the library providing custom initialization params and an handler where
+					// to receive event notifications. Following Voip methods are called from the handleMassage() callback method
+					//boolean result = myVoip.initLib(params, new RegistrationHandler(this, myVoip));
+					myVoip.initLib(this.getApplicationContext(), params, this.voipHandler);
+				}
+				else 
+					{
+					Log.d(TAG,"Voip is not null... Destroying the lib before reinitializing.....");
+					// Reinitialization will be done after deinitialization event callback
+					this.voipHandler.reinitRequest  = true;
+					myVoip.destroyLib();
+					}
+	}
+	
+	
+
+
+	private HashMap<String,String> getVoipSetupParams()
+	    { 
+		    this.sipServerIp = "192.168.1.1";
+		    this.sipServerPort="5060";
+	    	HashMap<String,String> params = new HashMap<String,String>();
+			params.put("sipServerIp",sipServerIp); 
+			params.put("sipServerPort",sipServerPort); // default 5060
+			params.put("turnServerIp",  sipServerIp);
+			params.put("sipServerTransport","tcp"); 
+			
+			/* ecografista 	*/
+			accountName = "ecografista";
+			params.put("userPwd","sha1$fdcad$659da6841c6d8538b7a10ca12aae");
+
+			
+			/* specialista	
+			accountName = "specialista";
+			params.put("userPwd","sha1$40fcf$4718177db1b6966f64d2d436f212"); // 
+		*/
+			
+			params.put("userName",accountName); // specialista
+			params.put("turnServerUser",accountName);  // specialista
+			params.put("turnServerPwd",accountName);  // specialista
+		 
+			
+			String onHoldSoundPath = Utils.getResourcePathByAssetCopy(this.getApplicationContext(), "", "test_hold.wav");
+			String onIncomingCallRingTonePath = Utils.getResourcePathByAssetCopy(this.getApplicationContext(), "", "ring_in_call.wav");
+			String onOutcomingCallRingTonePath = Utils.getResourcePathByAssetCopy(this.getApplicationContext(), "", "ring_out_call.wav");
+			
+			
+			params.put("onHoldSound", onHoldSoundPath);
+			params.put("onIncomingCallSound",onIncomingCallRingTonePath ); // onIncomingCallRingTonePath
+			params.put("onOutcomingCallSound",onOutcomingCallRingTonePath); // onOutcomingCallRingTonePath
+			
+			Log.d(TAG,"OnHoldSoundPath:" + onHoldSoundPath);
+			 
+			return params;
+	    	
+	    }
 	
 }
