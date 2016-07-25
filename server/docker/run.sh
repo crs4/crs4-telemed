82ecd920e2b679faf26fb6c787f1a539cb08402c
Vittorio Meloni <vittorio.meloni@crs4.it>
2016-07-25 10:37:35 +0200

    added devices to initial docker images

--- server/docker/run.sh
+++ server/docker/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DB_IP_ADDR=$1
-IP_ADDR=$(ifconfig eth0 | grep "inet addr:" | cut -d : -f 2 | cut -d " " -f 1)
+NET_IF=$2
+IP_ADDR=$(ifconfig $NET_IF | grep "inet addr:" | cut -d : -f 2 | cut -d " " -f 1)
 
 sed -i 's/tcp_listen.*/tcp_listen\t\t'$IP_ADDR:3478'/' /etc/restund.conf
 sed -i 's/turn_relay_addr[^6].*/turn_relay_addr\t\t'$IP_ADDR'/' /etc/restund.conf
