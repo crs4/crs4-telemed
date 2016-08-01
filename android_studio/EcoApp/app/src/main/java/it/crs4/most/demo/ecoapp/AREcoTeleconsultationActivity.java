07e70e6df66718efdc3648e22251953088af6068
Mauro Del Rio <mauro@crs4.it>
2016-08-01 11:43:35 +0200

    added hold - hangup call functionality by pressing media button (short press - long press)

--- android_studio/EcoApp/app/src/main/java/it/crs4/most/demo/ecoapp/AREcoTeleconsultationActivity.java
+++ android_studio/EcoApp/app/src/main/java/it/crs4/most/demo/ecoapp/AREcoTeleconsultationActivity.java
@@ -3,9 +3,14 @@ package it.crs4.most.demo.ecoapp;
 
 import android.app.ActivityManager;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ConfigurationInfo;
+import android.media.AudioManager;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,6 +18,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.View;
@@ -33,6 +39,8 @@ import org.artoolkit.ar.base.rendering.ARRenderer;
 import org.artoolkit.ar.base.rendering.gles20.ARRendererGLES20;
 
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import it.crs4.most.demo.ecoapp.models.Teleconsultation;
 import it.crs4.most.visualization.IStreamFragmentCommandListener;
@@ -60,6 +68,79 @@ public class AREcoTeleconsultationActivity extends BaseEcoTeleconsultationActivi
     private EditText coordX, coordY, coordZ;
     private HashMap<String, Mesh> meshes = new HashMap<>();
 
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        private boolean toggle = true;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "onReceive, intent.getAction() " + intent.getAction());
+            if(intent.getAction().equals("HOLDCALL")){
+                toggleHoldCall(toggle);
+                toggle = !toggle;
+            }
+            else if (intent.getAction().equals("HANGUP")){
+                hangupCall();
+            }
+        }
+    };
+
+
+    public static class RemoteControlReceiver extends BroadcastReceiver {
+        String TAG = "RemoteControlReceiver";
+        final static Timer timer = new Timer();
+        static long actionDownTime;
+        static boolean actionDownReceived = false;
+
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final Context ctx = context;
+            Log.d(TAG, "onReceive, intent.getAction() " + intent.getAction());
+
+            if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
+                Log.d(TAG, "OMG key pressed");
+                KeyEvent event = intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+                if (event == null) {
+                    context.sendBroadcast(new Intent("HOLDCALL"));
+                }
+                else{
+                    int keycode = event.getKeyCode();
+                    int action = event.getAction();
+                    long eventTime = event.getEventTime();
+
+                    Log.d(TAG, "eventTime " + eventTime);
+//                    if (keycode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE || keycode == KeyEvent.KEYCODE_HEADSETHOOK) {
+                        if (action == KeyEvent.ACTION_DOWN && ! actionDownReceived) {
+                            Log.d("EVENT", "ACTION_DOWN, eventTime " + eventTime);
+                            actionDownTime = eventTime;
+                            actionDownReceived = true;
+//                            timer.schedule(new TimerTask() {
+//                                @Override
+//                                public void run() {
+//                                    ctx.sendBroadcast(new Intent("HANGUP"));
+//                                }
+//                            }, 1000);
+                        }
+                        else if(action == KeyEvent.ACTION_UP){
+                            Log.d("EVENT", "ACTION_UP eventTime " + eventTime);
+                            Log.d("EVENT", "eventTime - actionDownTime " + (eventTime - actionDownTime));
+                            if (eventTime - actionDownTime < 1000){
+                                Log.d("EVENT", "HOLDCALL intent");
+                                context.sendBroadcast(new Intent("HOLDCALL"));
+                            }
+                            else{
+                                Log.d("EVENT", "HANGUP intent");
+                                ctx.sendBroadcast(new Intent("HANGUP"));
+                            }
+                            actionDownReceived = false;
+//                            timer.cancel();
+//                            context.sendBroadcast(new Intent("HOLDCALL"));
+                        }
+//                    }
+                }
+            }
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -77,6 +158,13 @@ public class AREcoTeleconsultationActivity extends BaseEcoTeleconsultationActivi
 //        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
 //        decorView.setSystemUiVisibility(uiOptions);
 
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE) ;
+        ComponentName componentName = new ComponentName(this, RemoteControlReceiver.class);
+        am.registerMediaButtonEventReceiver(componentName);
+
+        registerReceiver(broadcastReceiver, new IntentFilter("HOLDCALL"));
+        registerReceiver(broadcastReceiver, new IntentFilter("HANGUP"));
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ar_eco);
