afb3e51a1f6dae3c64dde18094365b9b79c2a529
Vittorio Meloni <vittorio.meloni@crs4.it>
2017-04-28 15:24:09 +0200

    Fixed path

--- server/docker/crs4-telemed/init_django.sh
+++ server/docker/crs4-telemed/init_django.sh
@@ -2,4 +2,4 @@
 cd /opt/crs4-telemed
 make devel
 make sync
-cd server/most && PYTHONPATH=.. python manage.py loaddata /opt/crs4-telemed/server/docker/django_initial.json
+cd server/most && PYTHONPATH=.. python manage.py loaddata /opt/crs4-telemed/server/docker/crs4-telemed/django_initial.json
